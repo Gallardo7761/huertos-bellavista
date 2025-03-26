ecf7c5068318a114ba22fc6eeefd955949c86dfb
Jose <jomaamga@outlook.es>
2025-03-26 04:14:33 +0100

    mockup msg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,10 +8,11 @@ const Header = () => {
     return (
         <header className={`text-center bg-img ${theme}`}>
             <div className="m-0 p-5 mask">
-                <div className="d-flex justify-content-center align-items-center h-100">
+                <div className="d-flex flex-column justify-content-center align-items-center h-100">
                     <Link to='/' className='text-decoration-none'>
                         <h1 className='header-title m-0 text-white shadowed'>Asociación Huertos La Salud - Bellavista</h1>
                     </Link>
+                    <h4 className='bg-dark rounded-3 m-0 mt-3 p-2 col-6 text-danger'>🚧 Esto es un mockup, próximamente estará disponible la web entera 🚧</h4>
                 </div>
             </div>
         </header>
