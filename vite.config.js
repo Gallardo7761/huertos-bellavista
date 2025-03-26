2cc203b4006289c21f73b7d5012a5da8bc60d9eb
Jose <jomaamga@outlook.es>
2025-03-26 04:06:40 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import cleanPlugin from 'vite-plugin-clean'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), cleanPlugin()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendors": ["react", "react-dom"],
+          "leaflet": ["leaflet"],
+        }
+      }
+    },
+  },
 })
